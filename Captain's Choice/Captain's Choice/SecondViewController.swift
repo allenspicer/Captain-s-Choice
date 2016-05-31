63ecb05dc6ff0ead27aa7f78abce0eb0d3f873fa
Allen Spicer <allenspicer@gmail.com>
2016-05-31 15:33:52 -0400

    Fresh Start

---
+++ Captain's Choice/Captain's Choice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Captain's Choice
+//
+//  Created by Allen Spicer on 5/31/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
