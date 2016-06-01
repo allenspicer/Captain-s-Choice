dc65755fedb4c7ed669873c8d3b026e1a7b1d7a2
Allen Spicer <allenspicer@gmail.com>
2016-06-01 16:43:58 -0400

    Team View sources Firebase

--- Captain's Choice/Captain's Choice/LeagueViewController.swift
+++ Captain's Choice/Captain's Choice/LeagueViewController.swift
@@ -12,37 +12,59 @@ import Firebase
 class LeagueViewController: UITableViewController {
 
     
-    var teamRanks  = ["1", "1", "1"]
-    var teamNames = ["Southside", "Southside", "Southside"]
-    var teamRecords = ["7-3", "7-3", "7-3",]
-    let ref = Firebase(url: "https://spicerwhisper-59eee.firebaseio.com/league/team/name")
+    var teamRecords:Array<String>  = ["0"]
+    var teamNames:Array<String>  = ["0"]
+    var teamRanks:Array<Int>  = [0]
+    let teamNameRef = Firebase(url: "https://spicerwhisper-59eee.firebaseio.com/league/team/name")
+    let teamRankRef = Firebase(url: "https://spicerwhisper-59eee.firebaseio.com/league/team/rank")
+    let teamRecordRef = Firebase(url: "https://spicerwhisper-59eee.firebaseio.com/league/team/record")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref.observeEventType(.Value, withBlock:{snapshot in
+        teamNameRef.observeEventType(.Value, withBlock:{snapshot in
            self.teamNames[0] = (snapshot.value) as! String
-            print(self.teamNames[0])
             self.tableView.reloadData()
             }, withCancelBlock: { error in
                 print(error.description)
                 
         })
+        teamRankRef.observeEventType(.Value, withBlock:{snapshot in
+            self.teamRanks[0] = (snapshot.value) as! Int
+            self.tableView.reloadData()
+            }, withCancelBlock: { error in
+                print(error.description)
+                
+        })
+        teamRecordRef.observeEventType(.Value, withBlock:{snapshot in
+            self.teamRecords[0] = snapshot.value as! String
+            self.tableView.reloadData()
+            }, withCancelBlock: { error in
+                print(error.description)
+                
+        })
+        
+ 
 }
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return teamNames.count
+        return 20
     }
     
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("TeamCell", forIndexPath: indexPath) as! TeamCell
         
-    cell.teamName.text = teamNames[indexPath.row]
-    cell.teamRank.text = teamRanks[indexPath.row]
-    cell.teamRecord.text = teamRecords[indexPath.row]
+        if (teamNames != ["0"]) {
+            cell.teamName.text = teamNames[0]}
+        
+        if (teamRanks != ["0"]){
+    cell.teamRank.text = String(teamRanks[0])}
+        
+        if (teamRecords != ["0"]) {
+            cell.teamRecord.text = teamRecords[0]}
         
         return cell
     }
